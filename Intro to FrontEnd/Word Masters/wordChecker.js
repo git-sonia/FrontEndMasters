c54568f9587bae84d5c13551723447c803f65b9c
Sonia <tsonia5987@gmail.com>
2023-01-19 17:38:27 -0500

    can input 1 word

--- Intro to FrontEnd/Word Masters/wordChecker.js
+++ Intro to FrontEnd/Word Masters/wordChecker.js
@@ -1,9 +1,10 @@
 let letterCount = 0;
+let rowCount = 0;
 const inputs = document.querySelector(".row").children;
 document
   .querySelector("body")
   .addEventListener("keyup", event => {
     console.log(letterCount);
-    inputs.item(letterCount).innerHTML = "2";
+    inputs.item(letterCount).innerHTML = event.key.toUpperCase();
     letterCount++;
   });
