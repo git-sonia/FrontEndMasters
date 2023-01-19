99406aa1ec096a734242d59712e789c913d5f9fe
Sonia <tsonia5987@gmail.com>
2023-01-19 12:40:45 -0500

    starting word masters

--- Intro to FrontEnd/Calculator/calculations.js
+++ Intro to FrontEnd/Calculator/calculations.js
@@ -1,5 +1,68 @@
+let result = "0";
+let numberInput = "";
+let operation = "";
+let resultArea = document.querySelector(".result");
 document
-  .querySelector(".button-container")
-  .addEventListener("click", function (event) {
-  alert(`You clicked on button ${event.target.innerText}`);
-});
+  .querySelectorAll(".positive-numbers, .zero")
+  .forEach(element => {
+    element.addEventListener("click", event => {
+      numberInput += event.target.innerText;
+      resultArea.innerText = numberInput;
+    })
+  });
+document
+  .querySelector(".operations")
+  .addEventListener("click", event => {
+    resultArea.innerText = "0";
+    if (operation === "+") {
+      result = add();
+    } else if (operation === "−") {
+      result = subtract();
+    } else if (operation === "×") {
+      result = multiply();
+    } else if (operation === "÷") {
+      result = divide();
+    } else {
+      result = numberInput;
+    }
+    operation = event.target.innerText;
+    if (operation === "=") {
+      resultArea.innerText = result;
+      numberInput = "" + result;
+    } else {
+      numberInput = "";
+    }
+  })
+document
+  .querySelector(".clear")
+  .addEventListener("click", event => {
+    result = "0";
+    resultArea.innerText = "0";
+    numberInput = "";
+    operation = "";
+  })
+document
+  .querySelector(".back-space")
+  .addEventListener("click", event => {
+    backSpace();
+  })
+const add = () => {
+  return parseInt(result) + parseInt(numberInput);
+}
+const subtract = () => {
+  return parseInt(result) - parseInt(numberInput);
+}
+const multiply = () => {
+  return parseInt(result) * parseInt(numberInput);
+}
+const divide = () => {
+  return parseInt(result) / parseInt(numberInput);
+}
+const backSpace = () => {
+  numberInput = numberInput.slice(0, -1);
+  if (numberInput === "") {
+    resultArea.innerText = "0";
+  } else {
+    resultArea.innerText = numberInput;
+  }
+}
