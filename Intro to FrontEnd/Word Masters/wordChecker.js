08d7a399bdb6feca7909d6841f89abfccec159d7
Sonia <tsonia5987@gmail.com>
2023-01-20 13:09:24 -0500

    added regex check

--- Intro to FrontEnd/Word Masters/wordChecker.js
+++ Intro to FrontEnd/Word Masters/wordChecker.js
@@ -1,10 +1,29 @@
 let letterCount = 0;
-let rowCount = 0;
-const inputs = document.querySelector(".row").children;
+let tries = 0;
+let word = "";
+const rows = document.querySelectorAll(".row");
 document
   .querySelector("body")
   .addEventListener("keyup", event => {
-    console.log(letterCount);
-    inputs.item(letterCount).innerHTML = event.key.toUpperCase();
-    letterCount++;
+    const inputs = rows[tries].querySelectorAll(".input");
+    const letter = event.key;
+    if (isLetter(letter)) {
+      word += letter;
+      inputs[letterCount].innerHTML = letter.toUpperCase();
+      letterCount++;
+      if (letterCount === 5) {
+        /*listen for enter*/
+        tries++;
+        letterCount = 0;
+      }
+      if (tries == 6) {
+        losingScreen();
+      }
+   }
   });
+  const isLetter = (letter) => {
+    return /^[a-zA-Z]$/.test(letter);
+  }
+  const losingScreen = () => {
+    alert("lost");
+  }
