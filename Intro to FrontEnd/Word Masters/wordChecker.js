f0d0e0aaf086b797d66d14426150b86425ffa7f0
Sonia <tsonia5987@gmail.com>
2023-01-20 16:05:13 -0500

    able to enter words

--- Intro to FrontEnd/Word Masters/wordChecker.js
+++ Intro to FrontEnd/Word Masters/wordChecker.js
@@ -7,23 +7,27 @@ document
   .addEventListener("keyup", event => {
     const inputs = rows[tries].querySelectorAll(".input");
     const letter = event.key;
-    if (isLetter(letter)) {
-      word += letter;
-      inputs[letterCount].innerHTML = letter.toUpperCase();
-      letterCount++;
-      if (letterCount === 5) {
-        /*listen for enter*/
-        tries++;
-        letterCount = 0;
-      }
+    if (letter === "Enter" && letterCount === 5) {
+      console.log("Enter");
+      letterCount = 0;
+      tries++;
       if (tries == 6) {
         losingScreen();
       }
-   }
+    } else if (letterCount < 5) {
+      if (isLetter(letter)) {
+        word += letter;
+        inputs[letterCount].innerHTML = letter.toUpperCase();
+        letterCount++; 
+      }
+    }
   });
   const isLetter = (letter) => {
     return /^[a-zA-Z]$/.test(letter);
   }
   const losingScreen = () => {
     alert("lost");
+  }
+  const isWord = (word) => {
+    return true;
   }
