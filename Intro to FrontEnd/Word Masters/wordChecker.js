7259c9480bd0eb1c1c31853235103092c881aa52
Sonia <tsonia5987@gmail.com>
2023-01-20 19:12:15 -0500

    post to validate api

--- Intro to FrontEnd/Word Masters/wordChecker.js
+++ Intro to FrontEnd/Word Masters/wordChecker.js
@@ -1,33 +1,72 @@
 let letterCount = 0;
 let tries = 0;
 let word = "";
+const wordLength = 5;
+const totalTries = 6;
 const rows = document.querySelectorAll(".row");
+const VALIDATE_WORD_URL = "https://words.dev-apis.com/validate-word";
+const WORD_OF_DAY_URL = "https://words.dev-apis.com/word-of-the-day";
+
+let wordOfTheDay = "";
+getWord().then(function (word) {
+  wordOfTheDay = word;
+});
+async function getWord() {
+  const promise = await fetch(WORD_OF_DAY_URL);
+  const wordObject = await promise.json();
+  let word = wordObject.word
+  return word;
+}
 document
   .querySelector("body")
   .addEventListener("keyup", event => {
     const inputs = rows[tries].querySelectorAll(".input");
     const letter = event.key;
-    if (letter === "Enter" && letterCount === 5) {
-      console.log("Enter");
-      letterCount = 0;
-      tries++;
-      if (tries == 6) {
-        losingScreen();
-      }
-    } else if (letterCount < 5) {
+    if (letter === "Enter" && letterCount === wordLength) {
+      checkWord(word);
+    } else if (letterCount < wordLength) {
       if (isLetter(letter)) {
-        word += letter;
-        inputs[letterCount].innerHTML = letter.toUpperCase();
-        letterCount++; 
+        inputLetter(letter, inputs);
       }
     }
   });
-  const isLetter = (letter) => {
-    return /^[a-zA-Z]$/.test(letter);
+function inputLetter(letter, inputs) {
+  word += letter;
+  inputs[letterCount].innerHTML = letter.toUpperCase();
+  letterCount++; 
+}
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+function losingScreen() {
+  alert("lost");
+}
+function checkWord(word) {
+  const wordObject = {
+    word: word
   }
-  const losingScreen = () => {
-    alert("lost");
+  isWord(wordObject).then(function (isWordResult) {
+    if (isWordResult) {
+      console.log("is word");
+      submitWord();
+    } else {
+      console.log("not word");
+    }
+  })
+}
+function submitWord() {
+  letterCount = 0;
+  tries++;
+  word = "";
+  if (tries === totalTries) {
+    losingScreen();
   }
-  const isWord = (word) => {
-    return true;
-  }
+} 
+async function isWord(wordObject) {
+  const promise = await fetch(VALIDATE_WORD_URL, {
+    method: "POST",
+    body: JSON.stringify(wordObject)
+  })
+  const validationJson = await promise.json();
+  return validationJson.validWord;
+}
